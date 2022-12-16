dafe68faec52aa859546d3ab5d59592e4c7d9be3
nandobfer <nandobfer@yahoo.com.br>
2022-12-16 16:41:27 -0300

    update: build scripts

--- build.ps1
+++ build.ps1
@@ -1,5 +1,3 @@
 yarn build
-Write-Output 'Uploading files to hostgator'
-scp -r -P 22022 build/* root@agenciazop.com.br:/home/bapkasorvetes/cupons_webserver/
-ssh hostgator "chown -R bapkasorvetes:bapkasorvetes /home/bapkasorvetes/cupons_webserver/* && screen -XS bapka-api quit"
-ssh hostgator "cd /home/bapkasorvetes/cupons_webserver/bapka-api/ && git pull && sh /scripts/fernando/bapka-api.sh"
+Write-Output 'Uploading files to oracle'
+scp -r build/* nandoburgos.dev:/var/www/nandoburgos.dev/
--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 yarn build
-echo 'Uploading files to hostgator'
+echo 'Uploading files to oracle'
 scp -r build/* nandoburgos.dev:/var/www/nandoburgos.dev/
 # ssh hostgator "chown -R bapkasorvetes:bapkasorvetes /home/bapkasorvetes/cupons_webserver/* && screen -XS bapka-api quit"
 # ssh hostgator "cd /home/bapkasorvetes/cupons_webserver/bapka-api/ && git pull && sh /scripts/fernando/bapka-api.sh"
