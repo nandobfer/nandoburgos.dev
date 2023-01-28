2e68767136b9b4863a67b787f59b01494e81b8c7
nandobfer <nandobfer@yahoo.com.br>
2023-01-28 16:46:33 -0300

    update: styled mobile home

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -26,7 +26,7 @@ export const Header = () => {
             </div>
             <div className="header-buttons">
                 <HeaderButton color={COLORS.blue} onClick={() => navigate('/projects')} >PROJETOS</HeaderButton>
-                <HeaderButton color={COLORS.red} onClick={() => navigate('/cheatsheet')}>CHEAT-SHEET</HeaderButton>
+                <HeaderButton color={COLORS.red} onClick={() => navigate('/cheatsheet')}>CHEATSHEET</HeaderButton>
                 <HeaderButton color={COLORS.yellow} onClick={() => navigate('/about')} > SOBRE</HeaderButton>
             </div>
             <div className="github-icon icon">
