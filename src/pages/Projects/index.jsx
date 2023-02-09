f029c07921254a264445cc1c54942ab14637fb66
nandobfer <nandobfer@yahoo.com.br>
2023-02-09 19:22:03 -0300

    feat: added programming icons to projects page

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { ProgrammingIcons } from '../../components/ProgrammingIcons';
 import './style.scss';
 
 export const Projects = () => {
@@ -10,6 +11,7 @@ export const Projects = () => {
     return (
         <div className="Projects-page">
             <h1>Still thinking in a fun way to display the projects</h1>
+            <ProgrammingIcons />
         </div>
     )
 }
