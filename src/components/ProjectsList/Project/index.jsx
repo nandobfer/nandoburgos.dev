1211992b05221471f493ddb6bde49920bc7e4a45
nandobfer <nandobfer@yahoo.com.br>
2023-02-16 21:28:38 -0300

    feat: added tech icons to projects

--- src/components/ProjectsList/Project/index.jsx
+++ src/components/ProjectsList/Project/index.jsx
@@ -3,8 +3,17 @@ import { useActiveProjectType } from "../../../hooks/useActiveProjectType"
 import './style.scss';
 
 const WebProject = ({ project }) => {
+    
     return (
         <div className="web-project">
+            <div className="title">
+                <div className="icons">
+                    {project?.technologies.map(technology => {
+                        return technology.icon
+                    })}
+                </div>
+                <h1>{project?.name}</h1>
+            </div>
             <iframe width={1000} height={500} src={project?.url} frameborder="0"></iframe>
         </div>
     )
@@ -13,6 +22,7 @@ const WebProject = ({ project }) => {
 const MobileProject = ({ project }) => {
     return (
         <div className="web-project">
+            <h1>on development</h1>
             <p>mobile</p>
             <p>{project?.name}</p>
         </div>
@@ -22,6 +32,7 @@ const MobileProject = ({ project }) => {
 const RpaProject = ({ project }) => {
     return (
         <div className="web-project">
+            <h1>on development</h1>
             <p>rpa</p>
             <p>{project?.name}</p>
         </div>
@@ -31,6 +42,7 @@ const RpaProject = ({ project }) => {
 const GameProject = ({ project }) => {
     return (
         <div className="web-project">
+            <h1>on development</h1>
             <p>game</p>
             <p>{project?.name}</p>
         </div>
@@ -44,7 +56,6 @@ export const Project = ({  }) => {
     
     return (
         <div className='Project-Component' >
-            <h1>on development</h1>
             {
             project_type.name == 'web' ? 
             <WebProject project={project} /> : 
