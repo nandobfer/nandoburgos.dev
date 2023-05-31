13d641513a01624127612f6ef76f8a7d4ade70c1
nandobfer <nandobfer@yahoo.com.br>
2023-05-31 12:48:26 -0300

    update: api url

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: "http://localhost:4103/api",
+    // baseURL: "http://localhost:4103/api",
     //baseURL: "http://192.168.15.19:4103/api",
-    // baseURL: "https://app.agenciaboz.com.br:4103/api",
+    baseURL: "https://app.agenciaboz.com.br:4103/api",
     timeout: 1000 * 10,
 })
