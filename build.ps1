41da0460741974403fe533c7c8518caef3657d89
nandobfer <nandobfer@yahoo.com.br>
2023-05-31 12:46:02 -0300

    feat: added build script

---
+++ build.ps1
@@ -0,0 +1,8 @@
+$user = "nandoburgos"
+$subdomain = "public_html"
+$path = "/home/$user/$subdomain"
+
+yarn build
+Write-Output 'Uploading build to server'
+scp -r -P 22022 build/* agenciaboz:$path
+ssh -p 22022 agenciaboz "chown -R $user`:$user $path/*"
