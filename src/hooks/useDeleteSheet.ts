fa7eb87f3746efeb3819eefa8e768308fb068c29
nandobfer <nandobfer@yahoo.com.br>
2023-06-02 02:45:38 -0300

    update: only authorized delete

--- src/hooks/useDeleteSheet.ts
+++ src/hooks/useDeleteSheet.ts
@@ -4,6 +4,7 @@ import { useApi } from "./useApi"
 import { useSheets } from "./useSheets"
 import { useTerminal } from "./useTerminal"
 import { useRefresh } from "./useRefresh"
+import { useAuthentication } from "./useAuthentication"
 
 export const useDeleteSheet = () => {
     const api = useApi()
@@ -11,8 +12,15 @@ export const useDeleteSheet = () => {
     const refresh = useRefresh()
     const { sheets } = useSheets()
     const { snackbar } = useSnackbar()
+    const { authentication } = useAuthentication()
 
     const deleteSheet = (command: string) => {
+        if (!authentication) {
+            terminal.setShell("")
+            terminal.setPlaceholder("unauthorized")
+            return
+        }
+
         const [_, id] = command.split(" ")
         const sheet_id = Number(id)
         const sheet: Sheet = sheets.filter((item) => item.id == sheet_id)[0]
